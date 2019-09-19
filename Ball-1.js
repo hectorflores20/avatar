a2021010201e6f30b375f1bdcc17ee080c57a291
Hectorflores20 <hector.espinozaflores@sjsu.edu>
2019-09-19 16:10:15 -0700

    love

---
+++ Ball-1.js
@@ -0,0 +1,17 @@
+class Ball {
+  constructor(_xPos, _yPos, _rotation){
+    this.xPos = _xPos;
+    this.yPos = _yPos;
+    this.rotation = _rotation;
+  } 
+  
+  display() {
+    
+  }
+  
+}
+// function drawBall(){
+  //fill(101, 88, 219)
+  //circle(width * .4, height * .05, width * .4,height * .3)
+  
+//}
